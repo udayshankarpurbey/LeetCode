d2c04be30b28ecfac916fc5971c3e09db470c0ca
Uday Shankar Purbey <144823786+udayshankarpurbey@users.noreply.github.com>
2026-09-21 17:54:19 +0530

    Time: 8 ms (22.22%), Space: 64.4 MB (11.11%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.ts
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.ts
@@ -1,18 +1,20 @@
 function findMatrix(nums: number[]): number[][] {
 
     const matrixTrack = new Map();
-    let matrix = [];
+    let obj = {};
     for (let i = 0; i < nums.length; i++) {
         if (matrixTrack.has(nums[i])) {
             matrixTrack.set(nums[i], matrixTrack.get(nums[i]) + 1);
         } else {
             matrixTrack.set(nums[i], 0);
         }
-        matrix[matrixTrack.get(nums[i])] =
-            matrix[matrixTrack.get(nums[i])] ?
-                [...matrix[matrixTrack.get(nums[i])], nums[i]] :
-                [nums[i]];
+
+        if (obj[matrixTrack.get(nums[i])]) {
+            obj[matrixTrack.get(nums[i])].push(nums[i]);
+        } else {
+            obj[matrixTrack.get(nums[i])] = [nums[i]];
+        }
 
     }
-    return matrix;
+    return Object.values(obj);
 };
